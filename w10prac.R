82cfabb5ecbbb907c10138bf0cd7b8d09d8603ec
皮心歌 <pixinge@pixingedeMacBook-Air.local>
2025-10-07 11:12:20 +1100

    updated x-axis

--- w10prac.R
+++ w10prac.R
@@ -4,6 +4,6 @@ subset_data <- subset(data, date >= "2020-07-01" & date <= "2020-12-31")
 ggplot(subset_data, aes(x = date, y = hospital_beds)) +
 	geom_line(color = "pink", size = 1) +
 	geom_point(color = "blue", size = 1.5) +
-	labs(title = "the number of hospital beds needed between July and December of 2020", x = "Date", y = "hospital beds needed") + 
+	labs(title = "the number of hospital beds needed between July and December of 2020", x = "Month", y = "hospital beds needed") + 
 	theme_minimal(base_size = 13)
 
