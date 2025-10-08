0a96a84c496d405049468a21a9920288c82ff79a
皮心歌 <pixinge@pixingedeMacBook-Air.local>
2025-10-08 12:46:43 +1100

    change color and size

--- w10prac.R
+++ w10prac.R
@@ -4,7 +4,7 @@ subset_data <- subset(data, date >= "2020-07-01" & date <= "2020-12-31")
 ggplot(subset_data, aes(x = date)) +
 	geom_line(y = hospital_beds, color = "red", size = 1) +
 	geom_line(y = critical, color = "pink", size = 1) +
-	geom_line(y = dead, color = "yellow", size = 1) +
+	geom_line(y = dead, color = "green", size = 1.5) +
 	geom_point(color = "blue", size = 1.5) +
 	labs(title = "the number of hospital beds needed between July and December of 2020", x = "Month", y = "hospital beds needed(red line)") +
 	scale_y_sqrt()
